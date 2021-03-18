b96c801b425dbfaab85006f0b012494ed50847a8
Miamer <aymeric.bertrand@gmail.com>
2021-03-18 16:43:57 +0100

    Google OK et Email pasword OK

--- src/App.js
+++ src/App.js
@@ -1,23 +1,141 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import firebase from "./database";
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [userData, setUserdata] = useState({});
+
+  var googleProvider = new firebase.auth.GoogleAuthProvider();
+  var facebookProvider = new firebase.auth.FacebookAuthProvider();
+  var twitterProvider = new firebase.auth.TwitterAuthProvider();
+
+  function userLogin() {
+    if (email === "" || password === "") {
+      console.log("Entrer email et/ou mot de passe ");
+    } else {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(email, password)
+        .then(async (res) => {
+          const idTokenResult = await firebase
+            .auth()
+            .currentUser.getIdTokenResult();
+          let user = await firebase.auth().currentUser;
+          setUserdata({
+            name: user.displayName,
+            email: user.email,
+            token: idTokenResult.token,
+            uid: user.iud,
+          });
+        })
+        .catch((error) => {
+          console.log(error.message);
+        });
+    }
+  }
+
+  function authLoginProv(provider) {
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(async (result) => {
+        let user = await firebase.auth().currentUser;
+        setUserdata({
+          name: user.displayName,
+          email: user.email,
+          token: user.token,
+          uid: user.iud,
+        });
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  function logout() {
+    firebase.auth().signOut();
+    console.log("déconnecté");
+    setUserdata({});
+    setEmail("");
+    setPassword("");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        Login:
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        {email}
+      </p>
+      <p>
+        Mot de passe:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        {password}
+      </p>
+      <button
+        onClick={() => {
+          userLogin();
+        }}
+      >
+        Connexion LoginMdp
+      </button>
+
+      <p>-----------------------------</p>
+
+      <button
+        onClick={() => {
+          authLoginProv(googleProvider);
+        }}
+      >
+        Google
+      </button>
+
+      <button
+        onClick={() => {
+          authLoginProv(facebookProvider);
+        }}
+      >
+        Facebook
+      </button>
+
+      <button
+        onClick={() => {
+          authLoginProv(twitterProvider);
+        }}
+      >
+        Twitter
+      </button>
+
+      <p>-----------------------------</p>
+
+      <p>userDataName: {userData.name}</p>
+      <p>userDataemail: {userData.email}</p>
+      <p>userDataToken: {userData.token}</p>
+      <p>userDatauid: {userData.uid}</p>
+
+      <p>-----------------------------</p>
+
+      <button
+        onClick={() => {
+          logout();
+        }}
+      >
+        LogOut
+      </button>
     </div>
   );
 }
---
+++ src/database.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase';
+import "firebase/auth";
+
+
+// Initialize Firebase
+const firebaseConfig = {
+
+    apiKey: process.env.REACT_APP_APIKEY,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_PROJECTID,
+    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_APPID
+
+
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+
+
+
+  export default firebase;
